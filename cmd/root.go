cd2ec20140e5a2bbc077a14f29a047ea02e69613
Konstantin Gasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2020-11-21 21:16:53 +0100

    fix long loading of image

--- cmd/root.go
+++ cmd/root.go
@@ -16,23 +16,41 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+var cfgFile string
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "icat",
-	Short: "icat lets you display images in your command-line",
-	Long: `With icat you can dispaly images in the command-line (iTerm2 only) 
-			or write the base64 encoded image to a file or to stdout`,
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.icat.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ internal/b64_io.go
@@ -0,0 +1,43 @@
+package internal
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// initLine tells iTerm to render base64 while finishLine tells when to stop
+var (
+	initLine   = strings.NewReader("\033]1337;File=inline=1:")
+	finishLine = strings.NewReader("\a")
+)
+
+func WriteView(dst io.Writer, src string) error {
+
+	f, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("Could not open the file at `%s`: %s", src, err.Error())
+	}
+	defer f.Close()
+
+	pipeReader, pipeWriter := io.Pipe()
+
+	// spin up gorutine to write base64 image in pipeWriter
+	go func() {
+		defer pipeWriter.Close()
+		// get base64 stream Writer
+		b64Stream := base64.NewEncoder(base64.StdEncoding, pipeWriter)
+		if _, err := io.Copy(b64Stream, f); err != nil {
+			fmt.Printf("Could not encode file to base64: %s", err.Error())
+			return
+		}
+	}()
+
+	// final copy to the passed io.Writer
+	if _, err := io.Copy(dst, io.MultiReader(initLine, pipeReader, finishLine)); err != nil {
+		return fmt.Errorf("Could not view src: %s", err.Error())
+	}
+	return nil
+}
